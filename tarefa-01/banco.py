3a8f6f5e9d3355a1165d84a304178e2d5caf0d4d
Breno <brenosmmaia@gmail.com>
2023-09-16 23:18:42 -0300

    adicionando tranferencia RPC no mesmo banco

--- tarefa-01/banco.py
+++ tarefa-01/banco.py
@@ -1,4 +1,6 @@
 from cliente import Cliente
+from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.server import SimpleXMLRPCRequestHandler
 
 class Banco:
     def __init__(self, nome: str):
@@ -6,16 +8,14 @@ class Banco:
         self.nome = nome
         self.contas = {}
 
-    def criar_conta(self, cliente: 'Cliente', numero_conta: str, saldo_inicial: float = 0):
+    def criar_cliente(self, nome: str):
+        """Cria um novo cliente e retorna o objeto Cliente."""
+        cliente = Cliente(nome)
+        return cliente
+    
+    def criar_conta(self, cliente: str, numero_conta: str, saldo_inicial: float = 0):
         """Cria uma nova conta no banco com um saldo inicial e adiciona a conta ao cliente."""
-        self.contas[numero_conta] = {'cliente': cliente.nome, 'saldo': saldo_inicial}
-
-    def depositar(self, numero_conta: str, valor: float):
-        """Deposita um valor em uma conta."""
-        if numero_conta in self.contas:
-            self.contas[numero_conta] += valor
-        else:
-            raise ValueError("Conta não encontrada")
+        self.contas[numero_conta] = {'cliente': cliente, 'saldo': saldo_inicial}
 
     def depositar(self, numero_conta: str, valor: float):
         """Deposita um valor em uma conta."""
@@ -24,13 +24,43 @@ class Banco:
         else:
             raise ValueError("Conta não encontrada")
 
-    def transferir(self, cliente: 'Cliente', conta_origem: str, banco_destino: 'Banco', conta_destino: str, valor: float):
+    def transferir(self, cliente: str, conta_origem: str, banco_destino: str, conta_destino: str, valor: float):
         """Transfere um valor de uma conta para outra."""
-        if cliente.nome == self.contas[conta_origem]['cliente']:
+        if banco_destino != self.nome: raise NotImplementedError("Apenas transferencias no mesmo banco")
+        
+        if cliente == self.contas[conta_origem]['cliente']:
             if conta_origem in self.contas and self.contas[conta_origem]['saldo'] >= valor:
                 self.contas[conta_origem]['saldo'] -= valor
-                banco_destino.depositar(conta_destino, valor)
+                self.depositar(conta_destino, valor)
+                return self
             else:
                 raise ValueError("Transferência inválida")
         else:
             raise ValueError("A conta de origem não pertence ao solicitante da transferência")
+
+
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
+
+# Instanciando banco
+banco_1 = Banco('Banco_1')
+
+# Instanciando cliente
+cliente_1 = Cliente('João')
+cliente_2 = Cliente('Maria')
+
+# Criando contas
+banco_1.criar_conta(cliente_1.nome, '123', 100)
+banco_1.criar_conta(cliente_2.nome, '456', 100)
+
+# Cria o servidor
+with SimpleXMLRPCServer(('localhost', 8080), requestHandler=RequestHandler) as server:
+    server.register_introspection_functions()
+
+    # Registra a função transferir
+    server.register_function(banco_1.transferir)
+
+    # Roda o servidor
+    print("Servidor RPC iniciado na porta 8080")
+    server.serve_forever()
+
